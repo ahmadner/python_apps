4836df3fe77360505af0647c10ee778d2cbbb2c4
AHMAD <57689837+ahmadner@users.noreply.github.com>
2020-06-01 15:52:59 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+print ("\n")
 username = input("Enter The Username: ")
 password = str (input("Enter The Password: "))
 count = 3  #num of trys
@@ -7,10 +8,16 @@ while username != "admin" or password != "123":
         print ("\n you try many time wrong try again later .\n")
         exit()
     else:    
-        print("\n wrong password or username , try again \n")
+        print("\nwrong password or username , try again \n")
+        print("you have a " +str (count)+ " of trys \n")
         username = input("Enter The Username: ")
         password = input("Enter The Password: ")
 while username == "admin" and password == "123":
         
     print ("\n welcome to your program \n")
     exit()
+#username = admin
+#password = 123
+#|===================|
+#| coder : ahmadNer  |
+#|===================|
